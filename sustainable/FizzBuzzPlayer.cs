c36f0579c41165e6356430eea2eefd25ecd01560
Robert McCoy <rmccoy@apterainc.com>
2017-06-28 09:34:01 -0400

    Lit the fuse and ran away

--- sustainable/FizzBuzzPlayer.cs
+++ sustainable/FizzBuzzPlayer.cs
@@ -1,45 +1,24 @@
-﻿namespace sustainable
+﻿using System.Collections.Generic;
+
+namespace sustainable
 {
     class FizzBuzzPlayer
     {
         public string Play(int input)
         {
-            string result = "";
-
-            /*
-             * if it's neither fizz nor buzz, return the number 
-             * as a string
-             */
-            if (input % 3 != 0 && input % 5 != 0)
+            if (IsDivisibleByValue(input, 3) && IsDivisibleByValue(input, 5))
             {
-                result = input.ToString();
-
+                return "FizzBuzz";
             }
 
-            else if (input % 3 != 0 || input % 5 != 0)
-            {
+            if (IsDivisibleByValue(input, 3)) return "Fizz";
+            if (IsDivisibleByValue(input, 5)) return "Buzz";
+            return input.ToString();
+        }
 
-                /*
-                 * if we've made it this far and it's not a multiple of 3
-                 * it's buzz:
-                 */
-                if (input % 3 != 0) result = "buzz";
-
-                /*
-                 * if we've made it this far and it's not a multiple of 3
-                 * it's buzz:
-                 */
-                else if (input % 5 != 0) { result = "fizz"; }
-
-
-            }
-            else
-            {
-                // WHAT'S MY NAME
-                return "fizzbuzz";
-            }
-
-            return result;
+        private bool IsDivisibleByValue(int testValue, int divisor)
+        {
+            return (testValue % divisor == 0);
         }
     }
 }
--- sustainable/The_fizz_buzz_player.cs
+++ sustainable/The_fizz_buzz_player.cs
@@ -9,12 +9,12 @@
 
         [TestCase(1, ExpectedResult = "1", TestName = "given 1, returns 1")]
         [TestCase(2, ExpectedResult = "2", TestName = "given 2, returns 2")]
-        [TestCase(3, ExpectedResult = "fizz", TestName = "given 3, returns fizz")]
-        [TestCase(5, ExpectedResult = "buzz", TestName = "given 5, returns buzz")]
-        [TestCase(9, ExpectedResult = "fizz", TestName = "given 9, returns fizz")]
-        [TestCase(10, ExpectedResult = "buzz", TestName = "given 10, returns buzz")]
-        [TestCase(15, ExpectedResult = "fizzbuzz", TestName = "given 15, returns fizzbuzz")]
-        [TestCase(90, ExpectedResult = "fizzbuzz", TestName = "given 90, returns fizzbuzz")]
+        [TestCase(3, ExpectedResult = "Fizz", TestName = "given 3, returns fizz")]
+        [TestCase(5, ExpectedResult = "Buzz", TestName = "given 5, returns buzz")]
+        [TestCase(9, ExpectedResult = "Fizz", TestName = "given 9, returns fizz")]
+        [TestCase(10, ExpectedResult = "Buzz", TestName = "given 10, returns buzz")]
+        [TestCase(15, ExpectedResult = "FizzBuzz", TestName = "given 15, returns fizzbuzz")]
+        [TestCase(90, ExpectedResult = "FizzBuzz", TestName = "given 90, returns fizzbuzz")]
         public string play(int i)
         {
             Arrange();
